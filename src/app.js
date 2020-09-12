34f0d044ffc4c6149c3278d9d21e47897be70bf8
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2020-09-12 10:04:40 +0530

    Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Vishnu C Prasad'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about' , {
+        title: 'About me',
+        name: 'Vishnu C Prasad'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'This is some helpful text.'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
