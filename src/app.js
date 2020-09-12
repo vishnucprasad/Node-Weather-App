4b42b08b027e184ee5afcc8ad288afa3fd8f2cd7
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2020-09-12 08:39:02 +0530

    Serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -3,19 +3,28 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello Express!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help Page');
+    res.send([{
+        name: 'Vishnu',
+        age: 20
+    }, {
+        name: 'Sreekutty',
+        age: 20
+    }]);
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.send('<h1>About Page</h1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Show weather');
+    res.send({
+        location: 'New York',
+        forecast: 'It is currently 30 degrees out. It feels like 26 degrees out.'
+    });
 });
 
 app.listen(3000, () => console.log('Server started on http://localhost:3000'));
